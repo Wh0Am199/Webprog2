7c5131d714b09a6fe30ec1647b165ffd20c7ec93
Wh0Am1 <balintb99@gmail.com>
2020-05-03 15:52:31 +0200

    Added login.php

---
+++ protected/user/login.php
@@ -0,0 +1,19 @@
+<form>
+  <div class="form-group row">
+    <label for="inputEmail3" class="col-sm-1 col-form-label">Username</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="inputEmail3" placeholder="Username" name="username">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="inputPassword3" class="col-sm-1 col-form-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword3" placeholder="Password" name="password">
+    </div>
+  </div>
+  <div class="form-group row">
+    <div class="col-sm-10">
+      <button type="submit" class="btn btn-primary" name="signin">Sign in</button>
+    </div>
+  </div>
+</form>
