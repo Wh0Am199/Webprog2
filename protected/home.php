d9de68940c8910b773e2876105194143583f9fd7
Wh0Am1 <balintb99@gmail.com>
2020-05-03 15:29:42 +0200

    Added home.php

---
+++ protected/home.php
@@ -0,0 +1,8 @@
+<div class="col-sm-12">
+      <h4><small>WELCOME TO ANYCAR</small></h4>
+      <hr>
+      <h2>What is AnyCar for?</h2>
+      <p>AnyCar was created for those, who would like to buy any car (like mentioned in the name) and we give them the opportunity to look around in our collection and see what we can offer. If someone finds his/her dream car, can buy it very easily, just have to reserve it and that's it!</p>
+      <br><br>
+      <p>Good Browsing!</p>
+</div>
