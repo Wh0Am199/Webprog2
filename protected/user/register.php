b154beb2e0e05c16eb3025ca6b97d88fcf143f7a
Wh0Am1 <balintb99@gmail.com>
2020-05-03 13:43:14 +0200

    Basic register page created

---
+++ protected/user/register.php
@@ -0,0 +1,58 @@
+<form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Email</label>
+      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Password</label>
+      <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+    </div>
+  </div>
+  <div class="form-group col-md-6">
+    <label for="inputAddress">Address</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+  </div>
+  <div class="form-group col-md-6">
+    <label for="inputAddress2">Address 2</label>
+    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputCity">City</label>
+      <input type="text" class="form-control" id="inputCity">
+    </div>
+    <div class="form-group col-md-4">
+      <label for="inputState">State</label>
+      <select id="inputState" class="form-control">
+        <option selected>Choose...</option>
+        <option>Bács-Kiskun</option>
+        <option>Baranya</option>
+        <option>Békés</option>
+        <option>Borsod-Abaúj-Zemplén</option>
+        <option>Csongrád</option>
+        <option>Fejér</option>
+        <option>Győr-Moson-Sopron</option>
+        <option>Hajdú-Bihar</option>
+        <option>Heves</option>
+        <option>Jász-Nagykun-Szolnok</option>
+        <option>Komárom-Esztergom</option>
+        <option>Nógrád</option>
+        <option>Pest</option>
+        <option>Somogy</option>
+        <option>Szabolcs-Szatmár-Bereg</option>
+        <option>Tolna</option>
+        <option>Vas</option>
+        <option>Veszprém</option>
+        <option>Zala</option>
+      </select>
+    </div>
+    <div class="form-group col-md-2">
+      <label for="inputZip">Zip</label>
+      <input type="text" class="form-control" id="inputZip">
+    </div>
+  </div>
+  <div class="form-group col-md-2">
+    <button type="submit" class="btn btn-primary btn-block">Register</button>
+  </div>
+</form>
