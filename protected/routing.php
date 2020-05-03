732e5e1881cdc730441ae08fbf4a2ee3b81fdf2e
Wh0Am1 <balintb99@gmail.com>
2020-05-03 13:42:28 +0200

    Created routing.php for navigating between pages

---
+++ protected/routing.php
@@ -0,0 +1,26 @@
+<?php
+
+	if (array_key_exists('page', $_GET) && !empty($_GET['page'])) {
+		
+		switch ($_GET['page']) {
+			case 'home':
+				require_once PROTECTED_DIR.'home.php';
+				break;
+			
+			case 'register':
+				require_once PROTECTED_DIR.'user/register.php';
+				break;
+			
+
+			default:
+				# code...
+				break;
+		}
+	}
+	else{
+		
+	}
+
+
+
+?>
