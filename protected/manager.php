0ee6fd5a54916a3e67b778f766d8d9e170ebff33
Wh0Am1 <balintb99@gmail.com>
2020-05-03 15:00:19 +0200

    User manager added
    Register, Login etc functions

---
+++ protected/manager.php
@@ -0,0 +1,60 @@
+<?php 
+function IsUserLoggedIn() {
+	return $_SESSION  != null && array_key_exists('uid', $_SESSION) && is_numeric($_SESSION['uid']);
+}
+
+function UserLogout() {
+	session_unset();
+	session_destroy();
+	header('Location: index.php');
+}
+
+function UserLogin($email, $password) {
+	$query = "SELECT uid, firstName, lastName, email, isAdmin FROM users WHERE email = :email AND password = :password";
+	$params = [
+		':email' => $email,
+		':password' => sha1($password)
+	]; 
+
+	require_once DATABASE_CONTROLLER;
+	$record = getRecord($query, $params);
+	if(!empty($record)) {
+		$_SESSION['uid'] = $record['uid'];
+		$_SESSION['firstName'] = $record['firstName'];
+		$_SESSION['lastName'] = $record['lastName'];
+		$_SESSION['email'] = $record['email'];
+		$_SESSION['isAdmin'] = $record['isAdmin'];
+		header('Location: index.php');
+	}
+	return false;
+}
+
+function UserRegister($username, $firstName, $lastName, $email, $password, $address, $additionalAddress, $birthDate, $city, $state, $zip, $isAdmin) {
+	$query = "SELECT uid FROM users WHERE email = :email";
+	$params = [ ':email' => $email ];
+
+	require_once DATABASE_CONTROLLER;
+	$record = getRecord($query, $params);
+	if(empty($record)) {
+		$query = "INSERT INTO users (username, firstName, lastName, email, password, address, additionalAddress, birthDate, city, state, zip, isAdmin) VALUES (:username, :first_name, :last_name, :email, :password, :address, :additionalAddress="", :birthDate, :city, :state, :zip, 0)";
+		$params = [
+			':username' => $username,
+			':first_name' => $firstName,
+			':last_name' => $lastName,
+			':email' => $email,
+			':password' => sha1($password),
+			':address' => $address,
+			':additionalAddress' => $additionalAddress,
+			':birthDate' => $birthDate,
+			':city' => $city,
+			':state' => $state,
+			':zip' => $zip
+		];
+
+		if(executeDML($query, $params)) 
+			header('Location: index.php?P=login');
+	} 
+	return false;
+}
+
+?>
