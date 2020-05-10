76f93929c96c4873581b1a8ff149c4958fdc2b42
Wh0Am1 <balintb99@gmail.com>
2020-05-10 13:42:49 +0200

    Added reservation route to routing.php

--- protected/routing.php
+++ protected/routing.php
@@ -3,6 +3,7 @@
 	if (array_key_exists('page', $_GET) && !empty($_GET['page'])) {
 		
 		switch ($_GET['page']) {
+
 			case 'home':
 				require_once PROTECTED_DIR.'home.php';
 				break;
@@ -19,10 +20,14 @@
 				IsUserLoggedIn() ? UserLogout() : header('Location: index.php'); 
 				break;
 
+			case 'reservation':
+				require_once PROTECTED_DIR.'user/reservation.php';
+				break;
+				
 			case 'vehicles':
 				require_once PROTECTED_DIR.'vehicles/availableVehicles.php';
 				break;
-				
+
 			case 'vehicleProfile':
 				require_once PROTECTED_DIR.'vehicles/vehicleProfile.php';
 				break;
@@ -30,10 +35,11 @@
 			case 'addVehicle':
 				require_once PROTECTED_DIR.'vehicles/addVehicle.php';
 				break;
-				
+
 			default:
 				require_once PROTECTED_DIR.'home.php';
 				break;
+
 		}
 	}
 	else{
